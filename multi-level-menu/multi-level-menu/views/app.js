dd3d4765f3efd7605e5c3f49dffac7e519ae2e15
Mike Graham <mgraham@technicalmedia.com>
2015-02-20 16:53:15 -0700

    got the menu to hide the non-visible ones :)

--- multi-level-menu/multi-level-menu/views/app.js
+++ multi-level-menu/multi-level-menu/views/app.js
@@ -6,16 +6,16 @@ define(["require", "exports", "aurelia-router", "./MultiLevelMenuPipelineStep"],
                 config.title = "Aurelia VS/TS";
                 config.addPipelineStep("modelbind", mlmps.MultiLevelMenuPipelineStep);
                 config.map([
-                    { route: ["", "home"], moduleId: "views/home", nav: true, title: "home" },
-                    { route: ["item-1"], moduleId: "views/item-1", nav: true, title: "item 1" },
-                    { route: ["item-1-1"], moduleId: "views/item-1-1", nav: true, title: "item 1.1" },
-                    { route: ["item-1-2"], moduleId: "views/item-1-2", nav: true, title: "item 1.2" },
-                    { route: ["item-2"], moduleId: "views/item-2", nav: true, title: "item 2" },
-                    { route: ["item-2-1"], moduleId: "views/item-2-1", nav: true, title: "item 2.1" },
-                    { route: ["item-2-2"], moduleId: "views/item-2-2", nav: true, title: "item 2.2" },
-                    { route: ["item-2-2-1"], moduleId: "views/item-2-2-1", nav: true, title: "item 2.2.1" },
-                    { route: ["item-2-2-2"], moduleId: "views/item-2-2-2", nav: true, title: "item 2.2.2" },
-                    { route: ["item-2-3"], moduleId: "views/item-2-3", nav: true, title: "item 2.3" }
+                    { route: ["", "home"], moduleId: "views/home", nav: true, title: "home", settings: { level: 0, show: true } },
+                    { route: ["item-1"], moduleId: "views/item-1", nav: true, title: "item 1", settings: { level: 0, show: true } },
+                    { route: ["item-1-1"], moduleId: "views/item-1-1", nav: true, title: "item 1.1", settings: { level: 1, show: false } },
+                    { route: ["item-1-2"], moduleId: "views/item-1-2", nav: true, title: "item 1.2", settings: { level: 1, show: false } },
+                    { route: ["item-2"], moduleId: "views/item-2", nav: true, title: "item 2", settings: { level: 0, show: true } },
+                    { route: ["item-2-1"], moduleId: "views/item-2-1", nav: true, title: "item 2.1", settings: { level: 1, show: false } },
+                    { route: ["item-2-2"], moduleId: "views/item-2-2", nav: true, title: "item 2.2", settings: { level: 1, show: false } },
+                    { route: ["item-2-2-1"], moduleId: "views/item-2-2-1", nav: true, title: "item 2.2.1", settings: { level: 2, show: false } },
+                    { route: ["item-2-2-2"], moduleId: "views/item-2-2-2", nav: true, title: "item 2.2.2", settings: { level: 2, show: false } },
+                    { route: ["item-2-3"], moduleId: "views/item-2-3", nav: true, title: "item 2.3", settings: { level: 1, show: false } }
                 ]);
             });
         }
