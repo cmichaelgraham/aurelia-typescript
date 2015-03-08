441d79e9107c8c92c562e76f879e7ce0e7dc3e61
Mike Graham <mgraham@technicalmedia.com>
2015-03-08 12:41:09 -0600

    break out lines for clairity

--- push-samples-to-gh-pages/push-samples.sh
+++ push-samples-to-gh-pages/push-samples.sh
@@ -1,4 +1,11 @@
 echo push samples
-cp -fR ../aurelia-vs-ts/aurelia-vs-ts/app.* ../aurelia-vs-ts/aurelia-vs-ts/index.html ../aurelia-vs-ts/aurelia-vs-ts/main.js ../aurelia-vs-ts/aurelia-vs-ts/views ../aurelia-vs-ts/aurelia-vs-ts/styles ../aurelia-vs-ts/aurelia-vs-ts/scripts ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/app.* ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/index.html ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/main.js ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/views ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/styles ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/scripts ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+
 
 echo pushed
