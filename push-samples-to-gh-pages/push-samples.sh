309f743a02fa18b14857d401cdc3d506d811b775
Mike Graham <mgraham@technicalmedia.com>
2015-03-08 12:49:33 -0600

    updated deployment script

--- push-samples-to-gh-pages/push-samples.sh
+++ push-samples-to-gh-pages/push-samples.sh
@@ -1,5 +1,6 @@
 echo push samples
 
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/app.* ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/index.html ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/main.js ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
@@ -7,5 +8,12 @@ cp -fR ../aurelia-vs-ts/aurelia-vs-ts/views ../../cmichaelgraham.github.io/aurel
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/styles ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/scripts ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+cp -fR ../code-sandbox/code-sandbox/index.html ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+cp -fR ../code-sandbox/code-sandbox/views ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+cp -fR ../code-sandbox/code-sandbox/styles ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+cp -fR ../code-sandbox/code-sandbox/scripts ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+cp -fR ../code-sandbox/code-sandbox/aurelia ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+cp -fR ../code-sandbox/code-sandbox/images ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
 
 echo pushed
