1a154efb7874fc9e4f005cf8c7aec22857da54c1
Mike Graham <mgraham@technicalmedia.com>
2015-03-10 10:51:58 -0600

    skel- nav-require-vs-ts browser support working

--- aurelia-require-bundle/bundle-and-copy-to-solutions.sh
+++ aurelia-require-bundle/bundle-and-copy-to-solutions.sh
@@ -6,10 +6,11 @@ cp -f aurelia-bundle.js aurelia-bundle.min.js ../aurelia-vs-ts/aurelia-vs-ts/scr
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../aurelia-vs-ts-tests/aurelia-vs-ts-tests/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../code-sandbox/code-sandbox/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../multi-level-menu/multi-level-menu/scripts/aurelia
-cp -f aurelia-bundle.js aurelia-bundle.min.js ../pwkad-aurelia-samples/pwkad-aurelia-samples/aurelia
-cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-gulp/aurelia
+cp -f aurelia-bundle.js aurelia-bundle.min.js ../pwkad-aurelia-samples/pwkad-aurelia-samples/scripts/aurelia
+cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-gulp/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/scripts/aurelia
-cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts-telerik/skel-nav-esri-vs-ts-telerik/aurelia
-cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/aurelia
-cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-require-webstorm-ts/aurelia
+cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../todo-require-vs-ts/todo-require-vs-ts/aurelia
+
+cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts-telerik/skel-nav-esri-vs-ts-telerik/aurelia
+cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-require-webstorm-ts/aurelia
