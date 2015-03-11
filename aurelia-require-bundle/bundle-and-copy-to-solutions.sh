405c33a76a33c966ac6fe684590e5b9bf3648a78
Mike Graham <mgraham@technicalmedia.com>
2015-03-10 20:33:37 -0600

    remove bundle copy for todo
    something in the new bundle broke the todo app

--- aurelia-require-bundle/bundle-and-copy-to-solutions.sh
+++ aurelia-require-bundle/bundle-and-copy-to-solutions.sh
@@ -10,7 +10,10 @@ cp -f aurelia-bundle.js aurelia-bundle.min.js ../pwkad-aurelia-samples/pwkad-aur
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-gulp/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/scripts/aurelia
-cp -f aurelia-bundle.js aurelia-bundle.min.js ../todo-require-vs-ts/todo-require-vs-ts/scripts/aurelia
+
+
+# add this back in when new release fixes problem that breaks todo app
+# cp -f aurelia-bundle.js aurelia-bundle.min.js ../todo-require-vs-ts/todo-require-vs-ts/scripts/aurelia
 
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts-telerik/skel-nav-esri-vs-ts-telerik/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-require-webstorm-ts/aurelia
