a56ad39fab27881d4aac34b9e33f43f21601b78b
Mike Graham <mgraham@technicalmedia.com>
2015-03-10 19:21:26 -0600

    fixing up todo and code sandbox

--- aurelia-require-bundle/bundle-and-copy-to-solutions.sh
+++ aurelia-require-bundle/bundle-and-copy-to-solutions.sh
@@ -10,7 +10,7 @@ cp -f aurelia-bundle.js aurelia-bundle.min.js ../pwkad-aurelia-samples/pwkad-aur
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-gulp/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/scripts/aurelia
-cp -f aurelia-bundle.js aurelia-bundle.min.js ../todo-require-vs-ts/todo-require-vs-ts/aurelia
+cp -f aurelia-bundle.js aurelia-bundle.min.js ../todo-require-vs-ts/todo-require-vs-ts/scripts/aurelia
 
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts-telerik/skel-nav-esri-vs-ts-telerik/aurelia
 cp -f aurelia-bundle.js aurelia-bundle.min.js ../skel-nav-require-webstorm-ts/aurelia
