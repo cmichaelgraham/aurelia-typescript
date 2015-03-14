e10fb85508c49ba490ba453a3745aea4ac75bf9a
Mike Graham <mgraham@technicalmedia.com>
2015-03-14 09:59:35 -0600

    publishing new code-sandbox
    also made the formatting nicer and suppressed the error messages when
    creating a directory that already exists

--- push-samples-to-gh-pages/push-samples.sh
+++ push-samples-to-gh-pages/push-samples.sh
@@ -1,6 +1,7 @@
 echo push samples
 
-mkdir ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+echo + aurelia-vs-ts
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts 2</dev/null
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/app.* ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/index.html ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/index-release.html ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
@@ -11,44 +12,53 @@ cp -fR ../aurelia-vs-ts/aurelia-vs-ts/views ../../cmichaelgraham.github.io/aurel
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/styles ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/scripts ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 
-mkdir ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+echo + code-sandbox
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox 2</dev/null
 cp -fR ../code-sandbox/code-sandbox/index.html ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+cp -fR ../code-sandbox/code-sandbox/index-release.html ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
+cp -fR ../code-sandbox/code-sandbox/index-latest.html ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
 cp -fR ../code-sandbox/code-sandbox/views ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
 cp -fR ../code-sandbox/code-sandbox/styles ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
 cp -fR ../code-sandbox/code-sandbox/scripts ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
 cp -fR ../code-sandbox/code-sandbox/images ../../cmichaelgraham.github.io/aurelia-typescript/code-sandbox
 
-mkdir ../../cmichaelgraham.github.io/aurelia-typescript/pwkad-aurelia-samples
+echo + pwkad-aurelia-samples
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/pwkad-aurelia-samples 2</dev/null
 cp -fR ../pwkad-aurelia-samples/pwkad-aurelia-samples/index.html ../../cmichaelgraham.github.io/aurelia-typescript/pwkad-aurelia-samples
 cp -fR ../pwkad-aurelia-samples/pwkad-aurelia-samples/views ../../cmichaelgraham.github.io/aurelia-typescript/pwkad-aurelia-samples
 cp -fR ../pwkad-aurelia-samples/pwkad-aurelia-samples/styles ../../cmichaelgraham.github.io/aurelia-typescript/pwkad-aurelia-samples
 cp -fR ../pwkad-aurelia-samples/pwkad-aurelia-samples/scripts ../../cmichaelgraham.github.io/aurelia-typescript/pwkad-aurelia-samples
 
-mkdir ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-gulp
+echo + skel-nav-esri-gulp
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-gulp 2</dev/null
 cp -fR ../skel-nav-esri-gulp/index.html ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-gulp
 cp -fR ../skel-nav-esri-gulp/views ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-gulp
 cp -fR ../skel-nav-esri-gulp/styles ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-gulp
 cp -fR ../skel-nav-esri-gulp/scripts ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-gulp
 
-mkdir ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-vs-ts
+echo + skel-nav-esri-vs-ts
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-vs-ts 2</dev/null
 cp -fR ../skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/index.html ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-vs-ts
 cp -fR ../skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/views ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-vs-ts
 cp -fR ../skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/styles ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-vs-ts
 cp -fR ../skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/scripts ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-esri-vs-ts
 
-mkdir ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-require-vs-ts
+echo + skel-nav-require-vs-ts
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-require-vs-ts 2</dev/null
 cp -fR ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/index.html ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-require-vs-ts
 cp -fR ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/views ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-require-vs-ts
 cp -fR ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/styles ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-require-vs-ts
 cp -fR ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/scripts ../../cmichaelgraham.github.io/aurelia-typescript/skel-nav-require-vs-ts
 
-mkdir ../../cmichaelgraham.github.io/aurelia-typescript/todo-require-vs-ts
+echo + todo-require-vs-ts
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/todo-require-vs-ts 2</dev/null
 cp -fR ../todo-require-vs-ts/todo-require-vs-ts/index.html ../../cmichaelgraham.github.io/aurelia-typescript/todo-require-vs-ts
 cp -fR ../todo-require-vs-ts/todo-require-vs-ts/views ../../cmichaelgraham.github.io/aurelia-typescript/todo-require-vs-ts
 cp -fR ../todo-require-vs-ts/todo-require-vs-ts/styles ../../cmichaelgraham.github.io/aurelia-typescript/todo-require-vs-ts
 cp -fR ../todo-require-vs-ts/todo-require-vs-ts/scripts ../../cmichaelgraham.github.io/aurelia-typescript/todo-require-vs-ts
 
-mkdir ../../cmichaelgraham.github.io/aurelia-typescript/multi-level-menu
+echo + multi-level-menu
+mkdir ../../cmichaelgraham.github.io/aurelia-typescript/multi-level-menu 2</dev/null
 cp -fR ../multi-level-menu/multi-level-menu/index.html ../../cmichaelgraham.github.io/aurelia-typescript/multi-level-menu
 cp -fR ../multi-level-menu/multi-level-menu/views ../../cmichaelgraham.github.io/aurelia-typescript/multi-level-menu
 cp -fR ../multi-level-menu/multi-level-menu/styles ../../cmichaelgraham.github.io/aurelia-typescript/multi-level-menu
