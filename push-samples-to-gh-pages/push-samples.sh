d09b935062a6dedbf17101004afaefc3cd76e20f
Mike Graham <mgraham@technicalmedia.com>
2015-03-14 09:40:17 -0600

    updated code-sandbox with aurelia latest

--- push-samples-to-gh-pages/push-samples.sh
+++ push-samples-to-gh-pages/push-samples.sh
@@ -3,7 +3,10 @@ echo push samples
 mkdir ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/app.* ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/index.html ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
-cp -fR ../aurelia-vs-ts/aurelia-vs-ts/main.js ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/index-release.html ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/index-latest.html ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/main-release.js ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
+cp -fR ../aurelia-vs-ts/aurelia-vs-ts/main-latest.js ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/views ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/styles ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
 cp -fR ../aurelia-vs-ts/aurelia-vs-ts/scripts ../../cmichaelgraham.github.io/aurelia-typescript/aurelia-vs-ts
