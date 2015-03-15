30ff0d3cac7265c10a9203973eccfdd4794c33ee
Mike Graham <mgraham@technicalmedia.com>
2015-03-15 11:20:14 -0600

    todo supports bundles
    todo supports both latest and release aurelia bundles (except that it
    has an error, so i'm suppressing the release bundle from copying for now
    until i figure out what's wrong)

--- aurelia-require-bundle/bundle-and-copy-to-solutions.sh
+++ aurelia-require-bundle/bundle-and-copy-to-solutions.sh
@@ -43,10 +43,7 @@ cp -f aurelia-bundle.js aurelia-bundle-latest.js aurelia-bundle.min.js ../pwkad-
 cp -f aurelia-bundle.js aurelia-bundle-latest.js aurelia-bundle.min.js ../skel-nav-esri-gulp/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle-latest.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/scripts/aurelia
 cp -f aurelia-bundle.js aurelia-bundle-latest.js aurelia-bundle.min.js ../skel-nav-require-vs-ts/skel-nav-require-vs-ts/scripts/aurelia
-
-
-# add this back in when new release fixes problem that breaks todo app
-# cp -f aurelia-bundle.js aurelia-bundle.min.js ../todo-require-vs-ts/todo-require-vs-ts/scripts/aurelia
+cp -f aurelia-bundle-latest.js aurelia-bundle.min.js ../todo-require-vs-ts/todo-require-vs-ts/scripts/aurelia
 
 cp -f aurelia-bundle.js aurelia-bundle-latest.js aurelia-bundle.min.js ../skel-nav-esri-vs-ts-telerik/skel-nav-esri-vs-ts-telerik/aurelia
 cp -f aurelia-bundle.js aurelia-bundle-latest.js aurelia-bundle.min.js ../skel-nav-require-webstorm-ts/aurelia
