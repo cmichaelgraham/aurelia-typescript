62235b9a1a4f1b9e121112eb8da1143ccf9b20b0
Mike Graham <mgraham@technicalmedia.com>
2015-03-21 12:16:05 -0600

    add more aurelia libs
    history, event-aggregator, path, and route-recognizer

--- aurelia-ts-lib/get-latest.sh
+++ aurelia-ts-lib/get-latest.sh
@@ -6,10 +6,13 @@ git clone git://github.com/aurelia/metadata
 git clone git://github.com/aurelia/dependency-injection
 git clone git://github.com/aurelia/router
 git clone git://github.com/aurelia/http-client
+git clone git://github.com/aurelia/path
+git clone git://github.com/aurelia/history
+git clone git://github.com/aurelia/event-aggregator
+git clone git://github.com/aurelia/route-recognizer
 
 # git clone git://github.com/aurelia/loader
 # git clone git://github.com/aurelia/loader-default
-# git clone git://github.com/aurelia/path
 # git clone git://github.com/aurelia/task-queue
 # git clone git://github.com/aurelia/logging
 # git clone git://github.com/aurelia/logging-console
@@ -32,5 +35,9 @@ cp -fR aurelia-latest-repos/metadata/src/* aurelia-latest/metadata
 cp -fR aurelia-latest-repos/dependency-injection/src/* aurelia-latest/dependency-injection
 cp -fR aurelia-latest-repos/router/src/* aurelia-latest/router
 cp -fR aurelia-latest-repos/http-client/src/* aurelia-latest/http-client
+cp -fR aurelia-latest-repos/path/src/* aurelia-latest/path
+cp -fR aurelia-latest-repos/history/src/* aurelia-latest/history
+cp -fR aurelia-latest-repos/event-aggregator/src/* aurelia-latest/event-aggregator
+cp -fR aurelia-latest-repos/route-recognizer/src/* aurelia-latest/route-recognizer
 
 rm -fR aurelia-latest-repos
