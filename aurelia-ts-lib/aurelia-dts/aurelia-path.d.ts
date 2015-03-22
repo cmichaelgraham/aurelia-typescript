7af2dc1f5a409dd8791853fadcd800716d05e7bd
Mike Graham <mgraham@technicalmedia.com>
2015-03-22 08:27:16 -0600

    crazy: update `path` public interface

---
+++ aurelia-ts-lib/aurelia-dts/aurelia-path.d.ts
@@ -0,0 +1,5 @@
+declare module 'aurelia-path' {
+	function relativeToFile(name: any, file: any): any;
+	function join(path1: any, path2: any): any;
+	function buildQueryString(a: any, traditional?: any): string;
+}
