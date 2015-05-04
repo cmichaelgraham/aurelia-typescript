44fd7619bee12e8e812718d7eb0d41b155dcc17d
Mike Graham <mgraham@technicalmedia.com>
2015-05-04 07:57:02 -0600

    1st cut: port skel-nav-esri-vs-ts to ts15

--- skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/views/welcome.js
+++ skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/views/welcome.js
@@ -1,11 +1,15 @@
 define(["require", "exports"], function (require, exports) {
     var Welcome = (function () {
         function Welcome() {
-            this.heading = "Welcome to the Aurelia Navigation App (VS/TS)!";
-            this.firstName = "John";
-            this.lastName = "Doe";
+            this.heading = 'Welcome to the Aurelia Navigation App!';
+            this.firstName = 'John';
+            this.lastName = 'Doe';
         }
         Object.defineProperty(Welcome.prototype, "fullName", {
+            //Getters can't be observed with Object.observe, so they must be dirty checked.
+            //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
+            //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
+            //@computedFrom('firstName', 'lastName')
             get: function () {
                 return this.firstName + " " + this.lastName;
             },
@@ -18,5 +22,13 @@ define(["require", "exports"], function (require, exports) {
         return Welcome;
     })();
     exports.Welcome = Welcome;
+    var UpperValueConverter = (function () {
+        function UpperValueConverter() {
+        }
+        UpperValueConverter.prototype.toView = function (value) {
+            return value && value.toUpperCase();
+        };
+        return UpperValueConverter;
+    })();
+    exports.UpperValueConverter = UpperValueConverter;
 });
-//# sourceMappingURL=welcome.js.map
