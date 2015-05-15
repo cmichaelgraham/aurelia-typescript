7d62c52260be9ab7c5cbec757aa9fa6e2e009017
Mike Graham <mgraham@technicalmedia.com>
2015-05-15 14:56:32 -0600

    porting multi-level-menu (rough cut)

--- multi-level-menu/multi-level-menu/views/MultiLevelMenuPipelineStep.ts
+++ multi-level-menu/multi-level-menu/views/MultiLevelMenuPipelineStep.ts
@@ -1,11 +1,11 @@
 ﻿import auf = require("aurelia-framework");
-import aur = require("aurelia-router");
-import mlmu = require("./MultiLevelMenuUtil");
+import {NavigationContext} from "aurelia-router";
+import {MultiLevelMenuUtil} from "./MultiLevelMenuUtil";
 
 export class MultiLevelMenuPipelineStep {
-    run(routingContext: aur.NavigationContext, next: { (): void; cancel(): void; }) {
-        var targetRouteIndex = mlmu.MultiLevelMenuUtil.getTargetRouteIndex(routingContext.router, routingContext.plan.default.config.moduleId);
-        mlmu.MultiLevelMenuUtil.setForTarget(routingContext.router, targetRouteIndex);
+    run(routingContext: NavigationContext, next: { (): void; cancel(): void; }) {
+        var targetRouteIndex = MultiLevelMenuUtil.getTargetRouteIndex(routingContext.router, routingContext.plan.default.config.moduleId);
+        MultiLevelMenuUtil.setForTarget(routingContext.router, targetRouteIndex);
         return next();
     }
 }
