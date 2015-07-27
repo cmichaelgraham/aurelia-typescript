2a5dc727ef920f3abd44c9a3f585b019e4813ca3
Mike Graham <mgraham@technicalmedia.com>
2015-07-26 21:26:43 -0600

    multi-level-menu working and updated with webcomponents.js

--- multi-level-menu/multi-level-menu/views/multi-level-menu-helper.js
+++ multi-level-menu/multi-level-menu/views/multi-level-menu-helper.js
@@ -1,4 +1,4 @@
-if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
     switch (arguments.length) {
         case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
@@ -6,7 +6,10 @@ if (typeof __decorate !== "function") __decorate = function (decorators, target,
         case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
     }
 };
-define(["require", "exports", "aurelia-framework", "./MultiLevelMenuUtil"], function (require, exports, aurelia_framework_1, MultiLevelMenuUtil_1) {
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+define(["require", "exports", "aurelia-framework", "aurelia-router", "./MultiLevelMenuUtil"], function (require, exports, aurelia_framework_1, aurelia_router_1, MultiLevelMenuUtil_1) {
     var MultiLevelMenuHelper = (function () {
         function MultiLevelMenuHelper() {
         }
@@ -14,7 +17,8 @@ define(["require", "exports", "aurelia-framework", "./MultiLevelMenuUtil"], func
             MultiLevelMenuUtil_1.MultiLevelMenuUtil.goUp(this.router);
         };
         __decorate([
-            aurelia_framework_1.bindable
+            aurelia_framework_1.bindable, 
+            __metadata('design:type', aurelia_router_1.Router)
         ], MultiLevelMenuHelper.prototype, "router");
         return MultiLevelMenuHelper;
     })();
