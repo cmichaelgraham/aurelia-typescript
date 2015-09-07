d2ee8cb76fb6f09cdc838462e66023107547383d
Mike Graham <mgraham@technicalmedia.com>
2015-09-07 10:28:31 -0600

    update .js files

--- skel-nav-esri-atom/views/welcome.js
+++ skel-nav-esri-atom/views/welcome.js
@@ -1,15 +1,11 @@
 define(["require", "exports"], function (require, exports) {
     var Welcome = (function () {
         function Welcome() {
-            this.heading = 'Welcome to the Aurelia Navigation App!';
-            this.firstName = 'John';
-            this.lastName = 'Doe';
+            this.heading = "Welcome to the Aurelia Navigation App (VS/TS)!";
+            this.firstName = "John";
+            this.lastName = "Doe";
         }
         Object.defineProperty(Welcome.prototype, "fullName", {
-            //Getters can't be observed with Object.observe, so they must be dirty checked.
-            //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-            //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
-            //@computedFrom('firstName', 'lastName')
             get: function () {
                 return this.firstName + " " + this.lastName;
             },
@@ -22,13 +18,4 @@ define(["require", "exports"], function (require, exports) {
         return Welcome;
     })();
     exports.Welcome = Welcome;
-    var UpperValueConverter = (function () {
-        function UpperValueConverter() {
-        }
-        UpperValueConverter.prototype.toView = function (value) {
-            return value && value.toUpperCase();
-        };
-        return UpperValueConverter;
-    })();
-    exports.UpperValueConverter = UpperValueConverter;
 });
